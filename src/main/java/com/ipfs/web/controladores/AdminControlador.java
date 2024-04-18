5d38007ff6c9923fbf3428a2488837fb1f4e1510
Manuel <manuel.machado.j.m@gmail.com>
2024-04-18 18:50:31 -0300

    actualizacion 18-04-24

--- src/main/java/com/ipfs/web/controladores/AdminControlador.java
+++ src/main/java/com/ipfs/web/controladores/AdminControlador.java
@@ -11,11 +11,14 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
 @RequestMapping("/admin")
 public class AdminControlador {
 
--- src/main/java/com/ipfs/web/servicios/ConductorServicio.java
+++ src/main/java/com/ipfs/web/servicios/ConductorServicio.java
@@ -5,7 +5,7 @@ import com.ipfs.web.excepciones.MiException;
 import com.ipfs.web.repositorios.ConductorRepositorio;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;;
+import java.util.Optional;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
